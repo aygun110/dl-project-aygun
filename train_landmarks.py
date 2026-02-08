fbd1837841a4c746dae2ce1877a871d2ef24362f
aygun110 <magerramova05@bk.ru>
2026-02-08 23:21:13 +0300

    Update train_landmarks.py

--- train_landmarks.py
+++ train_landmarks.py
@@ -3,15 +3,14 @@ from torch.utils.data import DataLoader
 from models.landmark_model import LandmarkNet
 from dataset_landmarks import CelebALandmarks
 
-# ------------------------------
 # 1. Настройка устройства
-# ------------------------------
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Используется устройство: {device}")
 
-# ------------------------------
+
 # 2. Загружаем датасет
-# ------------------------------
+
 dataset = CelebALandmarks(
     "data/img_align_celeba",
     "data/list_landmarks_align_celeba.txt"
@@ -21,16 +20,14 @@ loader = DataLoader(dataset, batch_size=64, shuffle=True)
 print(f"Длина датасета: {len(dataset)}")
 print(f"Количество батчей: {len(loader)}")
 
-# ------------------------------
 # 3. Создаём модель, loss и optimizer
-# ------------------------------
+
 model = LandmarkNet().to(device)
 criterion = torch.nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 
-# ------------------------------
 # 4. Обучение
-# ------------------------------
+
 num_epochs = 2
 for epoch in range(num_epochs):
     total_loss = 0
@@ -53,8 +50,8 @@ for epoch in range(num_epochs):
     avg_loss = total_loss / len(loader)
     print(f"Эпоха {epoch+1} завершена, Средняя потеря: {avg_loss:.4f}\n")
 
-# ------------------------------
+
 # 5. Сохраняем веса модели
-# ------------------------------
+
 torch.save(model.state_dict(), "landmark_model.pth")
 print("Модель успешно сохранена в landmark_model.pth")
