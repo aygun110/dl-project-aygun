e2d8b9414acd4f34c6a95270f517e4e6f513c3b5
aygun110 <magerramova05@bk.ru>
2026-02-08 23:22:49 +0300

    Update train_ce.py

--- train_ce.py
+++ train_ce.py
@@ -3,15 +3,13 @@ from torch.utils.data import DataLoader
 from dataset_identity import CelebAIdentity
 from models.facenet import FaceNet
 
-# ------------------------------
 # 1. Настройка устройства
-# ------------------------------
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Используется устройство: {device}")
 
-# ------------------------------
 # 2. Загружаем датасет
-# ------------------------------
+
 dataset = CelebAIdentity(
     "data/img_align_celeba",
     "data/identity_CelebA.txt"
@@ -23,16 +21,14 @@ print(f"Количество классов: {num_classes}")
 print(f"Длина датасета: {len(dataset)}")
 print(f"Количество батчей: {len(loader)}")
 
-# ------------------------------
 # 3. Создаём модель, loss и optimizer
-# ------------------------------
+
 model = FaceNet(num_classes=num_classes).to(device)
 criterion = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 
-# ------------------------------
 # 4. Обучение
-# ------------------------------
+
 num_epochs = 3
 for epoch in range(num_epochs):
     total_loss = 0
@@ -55,8 +51,8 @@ for epoch in range(num_epochs):
     avg_loss = total_loss / len(loader)
     print(f"Эпоха {epoch+1} завершена, Средняя потеря: {avg_loss:.4f}\n")
 
-# ------------------------------
+
 # 5. Сохраняем веса модели
-# ------------------------------
+
 torch.save(model.state_dict(), "face_ce.pth")
 print("Модель успешно сохранена в face_ce.pth")
